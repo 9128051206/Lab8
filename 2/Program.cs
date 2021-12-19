e46aebb144d5150dd523c4d2ce3925fae6ada454
91280 <91280@HP-ONQCSD8>
2021-12-20 00:28:02 +0500

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = File.CreateText("C:\\BIM\\F1.txt");
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(rnd.Next(100));
+                }
+            }
+            sw.Close();
+            int n, sum = 0;
+            string[] file_name = File.ReadAllLines(@"C:\\BIM\\F1.txt");
+            Console.Write("\nВыводим данные из файла :  \n" + file_name[0]);
+            n = Convert.ToInt32(file_name[0].ToString());
+            sum += n;
+            Console.Write("\nСумма : {0}\n", sum);
+            Console.ReadKey();
+        }
+    }
+}
+            
---
+++ Lab8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\AutoCAD");
+            Console.WriteLine("============Список каталогов=============");
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("==Список подкаталогов==");
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(it.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("==============Список файлов==============");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
